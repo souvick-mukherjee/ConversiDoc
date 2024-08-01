e65d9754fc56b0a8eb612f831411b376babed113
Souvick Mukherjee <souvick.mukherjee871@gmail.com>
2024-08-01 15:40:27 +0530

    download and delete doc features added

--- components/Document.tsx
+++ components/Document.tsx
@@ -1,7 +1,10 @@
 'use client';
 import { useRouter } from 'next/navigation';
-import React from 'react';
+import React, { useTransition } from 'react';
 import byteSize from 'byte-size';
+import { CloudIcon, Trash2Icon } from 'lucide-react';
+import { Button } from './ui/button';
+import { deleteDocument } from '@/actions/deleteDocument';
 
 function Document({
     id,
@@ -17,6 +20,8 @@ function Document({
 
 const router = useRouter();
 
+const [isDeleting, startTransaction] = useTransition();
+
   return (
     <div className='flex flex-col w-64 h-80 rounded-xl bg-white drop-shadow-md justify-between p-4 transition-all transform hover:scale-105 hover:bg-indigo-600 hover:text-white cursor-pointer group'>
         <div 
@@ -25,8 +30,40 @@ const router = useRouter();
             router.push(`/dashboard/files/${id}`);
         }}>
             <p className='font-semibold line-clamp-2'>{name}</p>
+           <p className='text-sm text-gray-500 group-hover:text-indigo-100' > 
             {/* Render size in KBs */}
             {byteSize(size).value} KB
+            </p>
+        </div>
+        {/* Actions */}
+        <div className='flex space-x-2 justify-end'>
+        <Button
+          variant="outline"
+          disabled={isDeleting }
+          onClick={() => {
+            const prompt = window.confirm(
+              "Are you sure you want to delete this document?"
+            );
+
+            if (prompt) {
+              // delete document
+              startTransaction(async () => {
+                await deleteDocument(id);
+              });
+            }
+          }}
+        >
+          <Trash2Icon className="h-6 w-6 text-red-500" />
+          
+        </Button>
+
+
+            <Button variant="outline" asChild>
+                <a href={downloadURL} download>
+                    <CloudIcon className='w-6 h-6 text-indigo-600'/>
+                </a>
+            </Button>
+
         </div>
     </div>
   )
